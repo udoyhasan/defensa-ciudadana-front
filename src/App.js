304ca0e37592ed9275de1d7138f259bd66fe01fa
Guillermo <guillermoPiedrabuena>
2020-01-13 19:54:20 -0300

    fixed a detail

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const buttonSet= (<Container colOrRow="flex-column" >
 </Container>);
 
 const panel=(            
-<div className="d-flex justify-content-center mt-5 pt-5 flex-column " style={{position: "absolute"}}>{store.getState().fetchedData[0]}</div>);
+<div className="d-flex mt-5 pt-5 flex-column " style={{position: "absolute"}}>{store.getState().fetchedData[0]}</div>);
 
 //COMPONENTE
 export class App extends React.Component {
@@ -29,15 +29,15 @@ export class App extends React.Component {
       <> 
         <div className="row mt-5 mp-5">
           <div className="col-md-3"></div>
-          <div className="col-md-3 d-flex justify-content-center ">
+          <div className="col-md-3 d-flex  ">
             <LogoGuide bootstrapClass="mr-5 align-items-end mt-5 mp-5"/>
           </div>
           <div className="col-md-4 mt-5 mp-5">
           {(this.props.boolean)? <div>
                             <Input />
-                              <div className="d-flex justify-content-center mt-2 pt-5 flex-column " style={{width: "100%", height: "310px", overflow: "auto"}}>
+                              <div className="d-flex  mt-2 flex-column " style={{width: "100%", height: "310px", overflow: "auto"}}>
                                 <h1 className="badge badge-secondary pt-2 pb-2" style={{fontSize: "100%", backgroundColor: "white", color: "black"}}>
-                                  {store.getState().fetchedData.resp.map((item, index)=>{return <button className="btn btn-secondary d-block mb-3 w-100" style={{width: "60%"}} key={index}>{item}</button>})}
+                                  {store.getState().fetchedData.resp.map((item, index)=>{return <button className="btn btn-secondary d-block mb-3 w-100" style={{width: "60%"}} key={index}>{item[0]}</button>})}
                                 </h1>
                               </div>
                             </div>
