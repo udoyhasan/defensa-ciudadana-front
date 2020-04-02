70dc26cfb29ad4bc5dce54821448157db330961e
Guillermo <guillermoPiedrabuena>
2020-04-02 13:58:40 -0300

    worcks with the back, pdf store

--- src/components/cpanel.js
+++ src/components/cpanel.js
@@ -9,9 +9,12 @@ export default class Cpanel extends React.Component{
     constructor(props){
         super(props);
         this.state = {dataList:[]}
+
         //FUNCIONES ENLAZADAS CON CLASE DE COMPONENTE
         this.postNewCaseAndClient=this.postNewCaseAndClient.bind(this);
         this.updateCase=this.updateCase.bind(this);
+        this.docSubmit=this.docSubmit.bind(this);
+
         //REFERENCIAS DE FORMULARIO PARA CREAR NUEVO CLIENTE
         this.nombre = React.createRef();
         this.rut = React.createRef();
@@ -25,12 +28,19 @@ export default class Cpanel extends React.Component{
         this.materia = React.createRef();
         this.procedimiento = React.createRef();
         this.objetivo = React.createRef(); 
+
         //REFERENCIAS DE FORMULARIO ACTUALIZACION CAUSA
+        this.dataListInput = React.createRef();
         this.ActualizacionAvanceCausa = React.createRef();
         this.modificacion_rol_rit_ruc = React.createRef();
         this.modificacion_juzgado_institucion = React.createRef();
         this.modificacion_descripcion = React.createRef();
-        this.causa_teminada_checkBox = React.createRef();
+        this.causa_teminada_checkBox = React.createRef(); 
+
+        //REFERENCIAS DOCUMENTO CARGADO
+        this.PDFfile = React.createRef();
+        //this.fechaFirma = React.createRef(); 
+        //this.tipo_doc = React.createRef();
     }
 
 postNewCaseAndClient()
@@ -51,7 +61,7 @@ postNewCaseAndClient()
      rol_rit_ruc: this.rol_rit_ruc.current.value,
      materia: this.materia.current.value,
      procedimiento: this.procedimiento.current.value,
-     objetivo: this.objetivo.current.value  
+     objetivo: this.objetivo.current.value
     };
     // request options
     const options = {
@@ -69,18 +79,30 @@ postNewCaseAndClient()
 }
 
 updateCase(){//FETCH WITH PUT METHOD TO UPDATE THE TABLE
- 
-    const urlClients = store.getState().fetchBase +'clientes/new'
+    
+    const urlClients = store.getState().fetchBase +'clientes/new'//FETCH CON POST A CLIENTES
+
+    //SE OBTIENE NOMBRE DEL INPUTDATA
+    let str = this.dataListInput.current.value;
+    let indx = str.indexOf("/");
+    let substr= str.slice(0,indx);
+
+    //SE OBTIENE CASO_ID DEL INPUTDATA
+    let str2 = this.dataListInput.current.value;
+    let indx2 = str2.indexOf("%");
+    let substr2= str2.slice(indx2+1, indx2+2);
 
     const clientData = {
-        //nombre: this.nombre.current.value,
-        rut: "6.717.203-5",
+        nombre: substr,
+        caso_id: substr2,
         actualizacion: (this.ActualizacionAvanceCausa.current.value === "")? "null" :this.ActualizacionAvanceCausa.current.value,
         caso_rol_rit_ruc: (this.modificacion_rol_rit_ruc.current.value == "")? "null" :this.modificacion_rol_rit_ruc.current.value,
         juzgado_institucion: (this.modificacion_juzgado_institucion.current.value == "")? "null" :this.modificacion_juzgado_institucion.current.value,
         descripcion: (this.modificacion_descripcion.current.value == "")? "null" :this.modificacion_descripcion.current.value,
         causaTerminada: this.causa_teminada_checkBox.current.checked.toString()
+ 
         };
+
        // request options
        const options = {
            method: 'PUT',
@@ -92,7 +114,26 @@ updateCase(){//FETCH WITH PUT METHOD TO UPDATE THE TABLE
 
     fetch(urlClients, options)
         .then(res => {return res.json()})
-        .then(data => console.log(JSON.stringify(data))); 
+        .then(data => JSON.stringify(data));
+     
+}
+
+docSubmit(){
+
+    const endpoint = store.getState().fetchBase + "prueba";
+    const pdf = this.PDFfile.current.files;
+    const formData = new FormData();
+
+    formData.append('pdf', pdf[0]);
+
+    fetch(endpoint,{
+
+        method: "POST",
+        body: formData
+    })
+    .then(res => {return res.json()})
+        .then(data => console.log(JSON.stringify(data.resp)));
+    
 }
 
 componentDidMount(){
@@ -108,16 +149,18 @@ componentDidMount(){
             
             fetch(store.getState().fetchBase + `casos/detalle/${item[3]}/${item[2]}`)//SE CARGA LOS DATOS DEL DATALIST
             .then(response => {return response.json();})
-            .then(data => {console.log(data)
+            .then(data => {
                 arr[index].push(data.resp[0][13]); //SE OBTIENE EL NOMBRE DEL CLIENTE EN EL CASO
                 this.setState({dataList:arr});
-                console.log(this.state)
+                console.table(this.state.dataList)
+                
         
         })
 
             
         })
-    })
+        })
+    
     
 }
 
@@ -159,21 +202,29 @@ render(){
         
                 <div className="mt-4" style={{backgroundColor: "#32D782", borderRadius: "10px", padding: "2%"}}>
                 <div className="h5" style={{color: "white",  fontWeight: "500", marginTop: "3%", textAlign: "center"}}>-ACTUALIZACIÓN CAUSA-</div>
-                    <input list="casos" style={{width: "100%", borderColor: "#4DF79F"}} ref={this.dataListINput}/>
+                    <input list="casos" style={{width: "100%", borderColor: "#4DF79F"}} ref={this.dataListInput}/>
                     <datalist id="casos" >
                     {this.state.dataList.map((item, index)=>{
-                            return <option key={index} value={`${item[4]}/${item[0]}/${item[1]}`} />
+                            return <option key={index} value={`${item[4]}/${item[1]} [${item[0]}]  %${item[2]}`} />//DEBES HACER QUE AL SELECCIONAR UNO SE TENGA EN CUENTA EL CASO_I Y EL CLIENTE_ID
                     })}
                     </datalist><br />
                     <textarea ref={this.ActualizacionAvanceCausa} className="mt-3" style={{width: "100%", borderColor: "#4DF79F"}} placeholder='  actualizar avance de la causa'/><br />
                     <input ref={this.modificacion_rol_rit_ruc} style={{width: "100%", marginTop:"3px", borderColor: "#4DF79F"}} placeholder='  modificar rol/rit/ruc causa'/><br />
                     <input ref={this.modificacion_juzgado_institucion} style={{width: "100%", marginTop:"3px", borderColor: "#4DF79F"}} placeholder='  modificar Juzgado/Institución'/><br />
                     <textarea ref={this.modificacion_descripcion} style={{width: "100%", marginTop:"3px", borderColor: "#4DF79F"}} placeholder='  modificar descripcion caso'/><br />
-                    <label class="checkbox-inline text-white font-weight-bold">Causa Terminada   <input ref={this.causa_teminada_checkBox} type="checkbox" style={{width: "20px", height: "20px"}}/></label>
+                    <label className="checkbox-inline text-white font-weight-bold">Causa Terminada   <input ref={this.causa_teminada_checkBox} type="checkbox" style={{width: "20px", height: "20px"}}/></label>
+
                     <input  style={{width: "100%", marginTop:"3px",  marginBottom: "3%", height: "50px", backgroundColor: "#6c757d", color: "white", fontWeight: "bold"}} id='actiualizar_causa' type='button' value='ACTUALIZAR CAUSA' onClick={this.updateCase}/>
                     
                 </div>
-
+{/**------------------------------------------------------------------------------------------------ */}
+                <div className="mt-4" style={{backgroundColor: "#32D782", borderRadius: "10px", padding: "2%"}}>
+                    <form>
+                        <input ref={this.PDFfile} type="file" accept=".pdf"/>    
+                        <input  onClick={this.docSubmit} value='CARGAR DOCUMENTO'  type='button' style={{width: "100%", marginTop:"3px",  marginBottom: "3%", height: "50px", backgroundColor: "#6c757d", color: "white", fontWeight: "bold"}}/>
+                    </form>
+                </div>
+{/**------------------------------------------------------------------------------------------------ */}
                 
             </div>
             <div className="col-md-1 mt-4"></div>
