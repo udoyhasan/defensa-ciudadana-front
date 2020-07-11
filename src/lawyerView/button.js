db5e86969f1afc90faea215f8d41559851d3f90f
Guillermo <guillermoPiedrabuena>
2020-07-11 00:50:16 -0400

    implemented the hooks efects to display case information

--- src/lawyerView/button.js
+++ src/lawyerView/button.js
@@ -7,7 +7,7 @@ export function Button(props){
 
         return( 
            <>   
-             <button type="button" className="btn btn-secondary d-block mb-3 w-100" style={{width: "60%", cursor: "pointer"}}>{props.children}</button>
+             <button id={props.id} onClick={props.click} type="button" className="btn btn-secondary d-block mb-3 w-100" style={{width: "60%", cursor: "pointer"}}>{props.children}</button>
 
            </> 
 
--- src/lawyerView/jobExchange.js
+++ src/lawyerView/jobExchange.js
@@ -1,5 +1,4 @@
 import React ,{ useState, useEffect} from 'react';
-import { connect } from 'react-redux';
 import {store} from '../redux/store.js';
 
 import {JobsContainer} from './JobsContainer.js'
@@ -7,25 +6,64 @@ import {Button} from './button.js'
 
 export function JobExchange(){
 
-    const [jobs, setJobs] = useState([]);
+    const [jobsList, setJobs] = useState([]);
+    const [selectedJobState, setSelectedJob] = useState(0);
+    const [displayedJob, setDisplayedJob] = useState({clients_name: " ", cases_legalIssue: " ", cases_description: " "});
 
 
-    useEffect(() => {//SE DEBE HACER FETCH DE CASOS
-        //setJobs([1,2])
-      });
+    useEffect(()=>{
+            fetch( store.getState().fetchBase + '/casos/17.402.744-7') 
+            .then(response => {return response.json();})
+            .then(data => {
+                setJobs([...data.resp])
+            })}, []
+      );
+
+      useEffect(()=>{
+
+        if(selectedJobState!=0){
+            
+            jobsList.forEach((item)=>{
+                //console.log("item.cases_id: " + item.cases_id)
+                //console.log("selectedJobHook: " + selectedJobHook)
+                if(item.cases_id==selectedJobState){
+                    setDisplayedJob({...item})
+                    
+                }
+
+        })
+      }
+       
+    },[selectedJobState]
+  );
+
+     
+    
+      const clickedJob = (e) =>{
+        
+        
+        setSelectedJob(e.target.id);//THE CASE ID
+        
+
+      }
+
 
         return(
            <>   
            <JobsContainer className='d-flex flex-row h-100'>
-           <h1 className='text-center mb-5'>BOLSA DE TRABAJO</h1>
+           <h1 className='text-center mb-5' >BOLSA DE TRABAJO</h1>
            <div className="d-flex flex-row bd-highlight mt-5 mb-3 h-100">
                 <div className="p-2 m-2 jumbotron w-50" style={{height: "29vh", overflowY: 'scroll'}}>
-                    {jobs.map((item) => <Button key={item}>{item}</Button>)}
+                {jobsList.map((item) => {
+                    if(item.cases_lawyer_id==1){
+                        return <Button click={clickedJob} id={item.cases_id} key={item.cases_id}>{item.cases_description}</Button>
+                    }
+                })}
                 </div>
-                <div className="p-2 m-2 jumbotron w-50 h-100" style={{height: "100%", color: 'white', backgroundColor: '#20be2b'}}>
-                    <h1>CLIENTE</h1>
-                    <p className="lead">MATERIA: <span>civil</span></p>
-                    <p className="lead">DESCRIPCIÓN: <span>obtener posesion efectiva</span></p>
+                <div className="p-2 pl-4 m-2 jumbotron w-50 h-100" style={{height: "100%", color: 'white', backgroundColor: '#20be2b'}}>
+            <h4 style={{minHeight: '5vh'}}>{displayedJob.clients_name.toUpperCase()}</h4>
+            <p className="lead">MATERIA: <span>{displayedJob.cases_legalIssue}</span></p>
+            <p className="lead">DESCRIPCIÓN: <span>{displayedJob.cases_description}</span></p>
                     <p className="lead">GANANCIA: <span>$200.000</span></p>
                 </div>
             </div>
