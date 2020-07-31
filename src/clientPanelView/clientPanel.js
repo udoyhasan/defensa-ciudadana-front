dbed850df90f2c2960c30d9a74948247cd68aa62
Guillermo <guillermoPiedrabuena>
2020-07-31 14:32:58 -0400

    working on landscape orientation on phone

--- src/clientPanelView/clientPanel.js
+++ src/clientPanelView/clientPanel.js
@@ -202,31 +202,31 @@ export class ClientPanel extends React.Component {
   render(){
     return (
       <> 
-      <div  ref={this.container} className='ml-5' style={{width: '100%' ,zIndex: '1', position: 'absolute', bottom:'-10%', transform: 'rotate(90deg)'}}></div>
-      <div className="container" style={{zIndex: '5', position: 'relative'}}>
+      <div  ref={this.container} className='ml-5 bubbles-background' style={{width: '100%' ,zIndex: '1', position: 'absolute', bottom:'-10%', transform: 'rotate(90deg)'}}></div>
+      <div className="container client-panel" style={{zIndex: '5', position: 'relative'}}>
         <div className="row">
           <div className="col-md-1"></div>
           <div className="col-md-10 text-justify">
           
-            <div className="jumbotron d-flex mb-0 pb-0 pt-3 mt-4 shadow-lg" style={{backgroundColor: "white", borderLeft: "100px solid #6c757d"}} >
+            <div className="jumbotron d-flex mb-0 pb-0 pt-3 mt-4 shadow-lg-custom" style={{backgroundColor: "white", borderLeft: "100px solid #6c757d"}} >
             <h1 className="display-4"></h1><br />
-            <div className="jumbotron w-100 pt-0 pb-0 pr-0 mr-0"  style={{backgroundColor: "white"}}>
+            <div className="jumbotron pt-0 pb-0 pr-0 mr-0 horizontal-landscape"  style={{backgroundColor: "white"}}>
               <h1 className="display-4 mb-0 text-left">{this.state.cases_description}</h1>
               <h6 className="text-left">{this.state.cases_client_name}</h6>
-              <div className="jumbotron p-3 pr-0 mr-0 mt-3 d-flex w-100"  style={{backgroundColor: "white"}}>
-    <div className="jumbotron p-0 pr-4 w-100"  style={{backgroundColor: "white"}}><p className="lead pr-5" style={{borderBottom: "2px solid rgb(3,104,10)"}}>AVANCE <span style={{fontSize: "10px"}}>{this.state.cases_updateDate}</span></p><span className="text-justify">{this.state.cases_update}</span></div>
+              <div className="jumbotron p-3 pr-0 mr-0 mt-3 d-flex w-100 "  style={{backgroundColor: "white"}}>
+          <div className="jumbotron p-0 pr-4 w-100 "  style={{backgroundColor: "white"}}><p className="lead pr-5" style={{borderBottom: "2px solid rgb(3,104,10)"}}>AVANCE <span style={{fontSize: "10px"}}>{this.state.cases_updateDate}</span></p><span className="text-justify">{this.state.cases_update}</span></div>
                 <div className="jumbotron p-0 w-50"  style={{backgroundColor: "white"}}><p className="lead" style={{borderBottom: "2px solid rgb(3,104,10)", }}>OBJETIVO</p><span style={{wordSpacing: "1px"}}>{this.state.cases_objetive}</span></div>
               </div>
               <div className="jumbotron p-0 w-100" style={{backgroundColor: "white"}}>
               <p className="lead text-left text-center" style={{borderBottom: "2px solid rgb(3,104,10)"}}>MI CAUSA</p>
               <div className="d-flex">
-  <div className="jumbotron p-0 w-100 " style={{backgroundColor: "white"}}><span className="badge badge-success h-100 mr-2">{this.state.cases_rol_rit_ruc} / {this.state.cases_trial_entity}</span></div>
+          <div className="jumbotron p-0 w-100 " style={{backgroundColor: "white"}}><span className="badge badge-success h-100 mr-2">{this.state.cases_rol_rit_ruc} / {this.state.cases_trial_entity}</span></div>
                 <div className="jumbotron p-0 w-100 " style={{backgroundColor: "white"}}><span className="badge badge-success h-100">{this.state.cases_legalIssue} / {this.state.cases_procedure}</span></div>
               </div>
             </div>
             </div>
             
-            <div className="jumbotron p-4 pt-5 d-flex w-50"  style={{backgroundColor: "white", marginTop: "10%"}}>
+            <div className="jumbotron p-4 pt-5 d-flex  docs-horizontal-landscape"  style={{backgroundColor: "white", marginTop: "10%"}}>
               <div className="list-group w-100" ref={this.documentListContainer} >
                 <a href="#" ref={this.dfPass} className="d-inline password list-group-item list-group-item-action border-0 active" style={{maxHeight: "15%",backgroundColor: "rgb(31,191,42)"}}>
                 <div className='text-center'>
